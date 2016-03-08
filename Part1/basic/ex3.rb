a43bd64d684b1395572fc47d3a8d92a578171978
J.CHO <jchobanana@gmail.com>
2016-03-08 10:23:27 +0800

    ex3

---
+++ Part1/basic/ex3.rb
@@ -0,0 +1,7 @@
+config = { "movie a" => 1975, "movie b" => 2004, "movie c" => 2013, "movie d" => 2001, "movie e" => 1981 }
+
+puts config["movie a"]
+puts config["movie b"]
+puts config["movie c"]
+puts config["movie d"]
+puts config["movie e"]
